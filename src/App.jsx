a3d1d4cf921e9c3bbff39ecca2afd69befde40ff
CrosscutX <crosscutcode@gmail.com>
2023-03-29 15:49:53 -0400

    Created components and set up home state.

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,17 @@
 import { useState } from "react";
 import "./style.css";
+import Header from "./Components/Header";
+import Home from "./Components/Home";
+import Footer from "./Components/Footer";
 
 export default function App() {
-  return <div className="app"></div>;
+  const [showHome, setShowHome] = useState(true);
+
+  return (
+    <div className="app">
+      <Header />
+      <Home />
+      <Footer />
+    </div>
+  );
 }
