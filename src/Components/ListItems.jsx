b39ee8f8efe5b571b7991cabcb9bf328b3851e6f
CrosscutX <crosscutcode@gmail.com>
2023-04-03 16:26:15 -0400

    Basic card formatting

--- src/Components/ListItems.jsx
+++ src/Components/ListItems.jsx
@@ -3,13 +3,17 @@ import React from "react";
 export default function ListItems(props) {
   return (
     <div className="list-items">
-      <div className="list-item-card">
-        <div className="image-container"></div>
+      <div className="list-items-card">
+        <img
+          src="https://www.defining.co/wp-content/uploads/2022/09/ScreenShot2020-12-26at5.06.58PM_2043ace9-22e9-4fd8-8532-cb9073b7385a.png"
+          alt=""
+        />
       </div>
       <div className="info-container">
-        <h2>Movie</h2>
-        <h3>Director</h3>
-        <h3>Score</h3>
+        <h3>John Wick 4</h3>
+        <div className="rating-container movie-rating-90">
+          <h4>94</h4>
+        </div>
       </div>
     </div>
   );
