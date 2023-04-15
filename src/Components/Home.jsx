57f96c48531407bd41170785ce2753a9a1d4fa71
Treyce Wheeler <t.zeryon@gmail.com>
2023-04-14 20:35:23 -0400

    Bugfixes for home searchbar and movie info components

--- src/Components/Home.jsx
+++ src/Components/Home.jsx
@@ -3,23 +3,57 @@ import SearchItems from "./SearchItems";
 
 export default function Home(props) {
   let movieItems = [];
+  console.log(props.searchResults);
+
+  props.searchResults.sort((next, prev) => {
+    //initial check for undefined ratings, sort at end if found.
+    if (next.Ratings[1] === undefined) {
+      return 1;
+    }
+
+    if (prev.Ratings[1] === undefined) {
+      return -1;
+    }
+    //Grab the previous and next rating values and turn them into numbers for comparison
+    let previousRating = prev.Ratings[1].Value;
+    previousRating = previousRating.substring(0, previousRating.length - 1);
+    previousRating = Number(previousRating);
+
+    let nextRating = next.Ratings[1].Value;
+    nextRating = nextRating.substring(0, nextRating.length - 1);
+    nextRating = Number(nextRating);
+    //-1 unshifts nextRating before previous, 1 pushes it after
+    if (previousRating > nextRating) {
+      return 1;
+    } else if (previousRating < nextRating) {
+      return -1;
+    } else {
+      return 0;
+    }
+  });
+
   function handleChange(event) {
     props.setSearchBar(event.target.value);
   }
 
   if (props.searchResults != undefined && props.searchResults.length != 0) {
-    movieItems = props.searchResults.map((movie) => {
-      return (
-        <SearchItems
-          key={movie.imdbID}
-          id={movie.imdbID}
-          showInfo={props.showInfo}
-          movie={movie}
-          setSelectedMovie={props.setSelectedMovie}
-          movieListAdd={props.movieListAdd}
-        />
-      );
-    });
+    if (props.homeSearchBar === "") {
+      movieItems = [];
+      props.setSearchResults([]);
+    } else {
+      movieItems = props.searchResults.map((movie) => {
+        return (
+          <SearchItems
+            key={movie.imdbID}
+            id={movie.imdbID}
+            showInfo={props.showInfo}
+            movie={movie}
+            setSelectedMovie={props.setSelectedMovie}
+            movieListAdd={props.movieListAdd}
+          />
+        );
+      });
+    }
   }
   return (
     <div className="home">
