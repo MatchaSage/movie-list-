127c7f3ca6cb32d68e96be8e320df5c611571baf
Treyce Wheeler <t.zeryon@gmail.com>
2023-04-17 20:09:24 -0400

    Styling added for movieinfo

--- src/Components/MovieInfo.jsx
+++ src/Components/MovieInfo.jsx
@@ -11,50 +11,67 @@ export default function MovieInfo(props) {
       return "";
     }
   }
-  console.log(window.innerWidth);
 
   return (
     <div className="infoDiv movieInfo">
       <button className="movieInfo--exitInfo" onClick={props.showInfo}>
         X
       </button>
-      <span className="infoDiv movieInfo--title">
+      <span className="movie-text infoDiv movieInfo--title">
         {props.selectedMovie.Title}
       </span>
-      <div className="infoDiv movieInfo--information-block">
-        <span className="infoDiv movieInfo--description">
+      <div className="movie-text infoDiv movieInfo--information-block">
+        <span className="movie-text infoDiv movieInfo--description">
           Description: {props.selectedMovie.Plot}
         </span>
-        <span className="infoDiv movieInfo--director">
+        <span className="movie-text infoDiv movieInfo--director">
           Director: {props.selectedMovie.Director}
         </span>
-        <span className="infoDiv movieInfo--writer">
+        <span className="movie-text infoDiv movieInfo--writer">
           Writer: {props.selectedMovie.Writer}
         </span>
-        <span className="infoDiv movieInfo--actors">
+        <span className="movie-text infoDiv movieInfo--actors">
           Actors: {props.selectedMovie.Actors}
         </span>
-        <span className="infoDiv movieInfo--realeaseDate">
+        <span className="movie-text infoDiv movieInfo--realeaseDate">
           Release: {props.selectedMovie.Released}
         </span>
-        <span className="infoDiv movieInfo--ratingRottenTomatoes">
+        <span className="movie-text infoDiv movieInfo--ratingRottenTomatoes">
           Rotten Tomatoes: &nbsp;
           {rating}
         </span>
-        <span className="infoDiv movieInfo--boxOffice">
+        <span className="movie-text infoDiv movieInfo--boxOffice">
           Boxoffice: {props.selectedMovie.BoxOffice}
         </span>
       </div>
 
       <div className="infoDiv movieInfo--buttons">
-        <button
-          className="infoDiv movieInfo--addMovie"
-          onClick={() => {
-            props.movieListAdd(props.selectedMovie);
-          }}
-        >
-          Add
-        </button>
+        {props.showPage != "list" && (
+          <button
+            className="infoDiv movieInfo--addMovie"
+            onClick={() => {
+              props.movieListAdd(props.selectedMovie);
+            }}
+          >
+            Add
+          </button>
+        )}
+
+        {props.showPage == "list" && (
+          <div className="infoDiv movieInfo--delete-watched">
+            <button
+              className="infoDiv movieInfo--deleteMovie"
+              onClick={() => {
+                props.movieListDelete(props.selectedMovie);
+              }}
+            >
+              Delete
+            </button>
+
+            <label htmlFor="watched">Watched</label>
+            <input htmlFor="watched" type="checkbox"></input>
+          </div>
+        )}
       </div>
     </div>
   );
