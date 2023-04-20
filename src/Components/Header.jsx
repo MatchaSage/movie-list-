400378eceedefe144bd8c8d1f56e68f4325d29a2
Treyce Wheeler <t.zeryon@gmail.com>
2023-04-20 15:32:33 -0400

    add icons to header

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -1,16 +1,19 @@
 import React from "react";
+import homeIcon from "../img/home.png";
+import shareIcon from "../img/share.png";
+import listIcon from "../img/list.png";
 
 export default function Header(props) {
   return (
     <header>
       <h2 className="home--button" onClick={props.showHome}>
-        Home
+        <img className="home-img" src={homeIcon} alt="Home" />
       </h2>
       <h2 className="list--button" onClick={props.showList}>
-        My List
+        <img className="list-img" src={listIcon} alt="List" />
       </h2>
       <h2 className="share--button" onClick={props.showShare}>
-        Share
+        <img className="share-img" src={shareIcon} alt="Share" />
       </h2>
     </header>
   );
