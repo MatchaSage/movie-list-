1e8b3a45aa9388b5265833c3320e372cec455413
CrosscutX <crosscutcode@gmail.com>
2023-04-20 15:14:29 -0400

    Home/list mobile styling and a jsx truncation fix.

--- src/Components/SearchItems.jsx
+++ src/Components/SearchItems.jsx
@@ -47,7 +47,7 @@ export default function searchItems(props) {
   function truncateTitle(title) {
     let mobileWidth = window.innerWidth;
 
-    if (mobileWidth <= 400) {
+    if (mobileWidth <= 400 && title.length > 20) {
       return `${title.slice(0, 20)}...`;
     } else if (title.length > 35) {
       return `${title.slice(0, 35)}...`;
