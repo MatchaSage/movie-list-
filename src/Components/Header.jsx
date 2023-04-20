06d1a3867d1e43c2c5e9fe0cc10f9fb48a795f0d
CrosscutX <crosscutcode@gmail.com>
2023-04-20 15:48:14 -0400

    Icon code fixes

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -6,15 +6,24 @@ import listIcon from "../img/list.png";
 export default function Header(props) {
   return (
     <header>
-      <h2 className="home--button" onClick={props.showHome}>
-        <img className="home-img" src={homeIcon} alt="Home" />
-      </h2>
-      <h2 className="list--button" onClick={props.showList}>
-        <img className="list-img" src={listIcon} alt="List" />
-      </h2>
-      <h2 className="share--button" onClick={props.showShare}>
-        <img className="share-img" src={shareIcon} alt="Share" />
-      </h2>
+      <img
+        className="home-img"
+        src={homeIcon}
+        alt="Home"
+        onClick={props.showHome}
+      />
+      <img
+        className="list-img"
+        src={listIcon}
+        alt="List"
+        onClick={props.showList}
+      />
+      <img
+        className="share-img"
+        src={shareIcon}
+        alt="Share"
+        onClick={props.showShare}
+      />
     </header>
   );
 }
